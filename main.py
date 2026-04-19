b383753d98df4ece40a9403b02cd00d55c8efa3a
Alexander Rodriguez <energia.alexander@gmail.com>
2026-04-19 09:06:49 -0500

    Version funcional modo seguro

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import os
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "SISTEMA N.O.V.A. ONLINE - MODO SEGURO"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
